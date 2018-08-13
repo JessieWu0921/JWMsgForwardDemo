5665b65aa10736ef5843b37bd33b723b82ee403a
JessieWu <jessiewu@1v1deMacBook-Air.local>
2018-08-13 17:39:47 +0800

    update

--- JWRuntimeMethodDemo/ViewController.m
+++ JWRuntimeMethodDemo/ViewController.m
@@ -37,6 +37,7 @@
 #pragma mark - override methods
 //3.消息转发 可以挽回查无此函数异常的最后一步：在实际应用中要实现该函数的调起 必须要重写methodSignatureForSelector:
 - (void)forwardInvocation:(NSInvocation *)anInvocation {
+    NSLog(@"%s", __func__);
     SEL selector = anInvocation.selector;
     JWFriend *friend = [JWFriend new];
     if ([friend respondsToSelector:selector]) {
@@ -47,22 +48,32 @@
 }
 
 //1.objc_msgSend 在searchMethod找不到SEL后，首先尝试在这里解决
-//+ (BOOL)resolveInstanceMethod:(SEL)sel {
-//    if (sel_isEqual(sel, @selector(sayHello))) {
-//        ViewController *vc = [ViewController new];
-//        [vc insteadSayHello];
-//        return YES;
-//    }
-//
-//    return NO;
-//}
++ (BOOL)resolveInstanceMethod:(SEL)sel {
+    NSLog(@"%s", __func__);
+    if (sel_isEqual(sel, @selector(sayHello))) {
+        class_addMethod([self class], sel, (IMP)insteadSayHello, "v@:");
+        return YES;
+    }
 
-//2.在上一步resloveInstanceMethod: or resloveClassMethod: return NO，会进入该函数查询SEL；在该函数未有结果后，正式进入objc_sendForword消息转发
-//- (id)forwardingTargetForSelector:(SEL)aSelector {
-//    NSLog(@"%s", __func__);
-//    NSMethodSignature *methodSign = [NSMethodSignature methodSignatureForSelector:aSelector];
-//    return [NSInvocation invocationWithMethodSignature:methodSign];
-//}
+    return [super resolveInstanceMethod:sel];
+}
+
+/**2.
+     *在上一步resloveInstanceMethod: or resloveClassMethod: return NO，会进入该函数查询SEL；
+     *该方法提供一次转移target的机会；
+     *在该函数未有结果后，正式进入objc_sendForword消息转发
+ **/
+- (id)forwardingTargetForSelector:(SEL)aSelector {
+    NSLog(@"%s", __func__);
+    if (sel_isEqual(aSelector, @selector(sayHello))) {
+        JWFriend *friend = [JWFriend new];
+        if ([friend respondsToSelector:aSelector]) {
+            return friend;
+        }
+    }
+    
+    return [super forwardingTargetForSelector:aSelector];
+}
 
 - (NSMethodSignature *)methodSignatureForSelector:(SEL)aSelector {
     if (sel_isEqual(aSelector, @selector(sayHello))) {
@@ -82,12 +93,13 @@
         for (; i < methodCount; i++) {
             NSLog(@"%@ - %@", [NSString stringWithCString:class_getName(currentClass) encoding:NSUTF8StringEncoding], [NSString stringWithCString:sel_getName(method_getName(methodList[i])) encoding:NSUTF8StringEncoding]);
         }
-        currentClass = self.superclass;
+        currentClass = [class superclass];
     }
 }
 
-- (void)insteadSayHello {
-    NSLog(@"%s", __func__);
+//instead function by C
+void insteadSayHello(id cls, SEL _cmd) {
+    printf("Hello hello");
 }
 
 #pragma mark - selector methods
